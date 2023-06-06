f98b4184ad64d58d7d103342ccbabd3c7a29fd51
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-06-06 15:05:51 +0200

    ADD file download in email

--- modal-form-script.js
+++ modal-form-script.js
@@ -1,12 +1,36 @@
 window.addEventListener('load', function () {
-  const brochureLinks = document.querySelectorAll('a[href="#brochure"]')
+  const brochureLinks = document.querySelectorAll('a[href*="#brochure"]')
+  console.log(brochureLinks)
   const modalForm = document.querySelector('#modal-form')
   const modalFormClose = document.querySelector('#modal-form-close')
+  let file; // variable for storing the file URL
 
   if (brochureLinks && modalForm && modalFormClose) {
     brochureLinks.forEach(function (element) {
       element.addEventListener('click', function (e) {
         e.preventDefault()
+
+        // Retrieve the full href including query parameters
+        const href = element.getAttribute('href');
+
+        // Check if href contains '?'
+        if (href.includes('?')) {
+          // Split the href on the '?' character to get the parameters
+          const parts = href.split('?');
+
+          // Check if we have parameters
+          if (parts.length > 1) {
+            // Split the parameters on the '=' character to get the file URL
+            const params = parts[1].split('=');
+
+            // Check if we have a file parameter
+            if (params[0] === 'file') {
+              file = params[1];
+              console.log(file);
+            }
+          }
+        }
+
         modalForm.style.display = 'block'
         document.body.style.overflow = 'hidden'
       })
@@ -32,14 +56,17 @@ window.addEventListener('load', function () {
       e.preventDefault()
       const formData = new FormData(form)
 
+      // Add the file URL to the form data
+      if (file) {
+        formData.append('file', file);
+      }
+
       let xhr = new XMLHttpRequest();
       xhr.open('POST', form.action, true);
 
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-          const html = xhr.responseText;
-          console.log(html);
-          modalForm.querySelector('.message').innerHTML = html;
+          modalForm.querySelector('.message').innerHTML = xhr.responseText;
         }
       };
 
