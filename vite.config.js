b723970cc6835ec2f6db2db5d90d7586ecfc0560
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-06-09 16:54:13 +0200

    FIX eslint & js code style

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
-export default ({
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+import { defineConfig } from "vite"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
   build: {
     lib: {
-      entry: "index.js",
-      name: "Modal Form Brochure"
+      entry: `${__dirname}/index.js`,
+      name: "Modal Form Brochure",
+      formats: ["es"] // Spécifiez uniquement 'es' pour n'obtenir que le format ESM (.mjs)
     },
-    outDir: "dist"
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].mjs", // Spécifiez explicitement .mjs comme extension de sortie
+        chunkFileNames: "[name].mjs",
+        assetFileNames: "[name].mjs"
+      }
+    }
   }
 })
