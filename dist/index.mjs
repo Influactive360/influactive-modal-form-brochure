b3df7b611986bb4d3b8dcba202e44e90e8d2d8e9
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-06-16 16:37:26 +0200

    UPDATE script

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,32 +1,32 @@
 window.addEventListener("load", function() {
-  const r = document.querySelectorAll('a[href*="#brochure"]'), e = document.querySelector("#modal-form"), c = document.querySelector("#modal-form-close");
-  let d;
-  console.log(e), console.log(getComputedStyle(e).display), e && getComputedStyle(e).display === "block" && (document.body.style.overflow = "hidden"), r && e && c && (r.forEach(function(t) {
-    t.addEventListener("click", function(l) {
-      l.preventDefault();
-      const o = t.getAttribute("href");
-      if (o.includes("?")) {
-        const s = o.split("?");
+  const r = document.querySelectorAll('a[href*="#brochure"]'), t = document.querySelector("#modal-form"), c = document.querySelector("#modal-form-close");
+  let l;
+  t && getComputedStyle(t).display === "block" && (document.body.style.overflow = "hidden"), r && t && c && (r.forEach(function(o) {
+    o.addEventListener("click", function(d) {
+      d.preventDefault();
+      const e = o.getAttribute("href");
+      if (e.includes("?")) {
+        const s = e.split("?");
         if (s.length > 1) {
           const i = s[1].split("=");
-          i[0] === "file" && (d = i[1]);
+          i[0] === "file" && (l = i[1]);
         }
       }
-      e.style.display = "block", document.body.style.overflow = "hidden";
+      t.style.display = "block", document.body.style.overflow = "hidden";
     });
   }), c.addEventListener("click", function() {
-    e.style.display = "none", document.body.style.overflow = "auto", document.body.style.overflowX = "hidden";
-  }), window.addEventListener("click", function(t) {
-    t.target === e && (e.style.display = "none", document.body.style.overflow = "auto", document.body.style.overflowX = "hidden");
+    t.style.display = "none", document.body.style.overflow = "auto", document.body.style.overflowX = "hidden";
+  }), window.addEventListener("click", function(o) {
+    o.target === t && (t.style.display = "none", document.body.style.overflow = "auto", document.body.style.overflowX = "hidden");
   }));
   const n = document.querySelector("#modal-form form");
-  n && n.addEventListener("submit", function(t) {
-    t.preventDefault();
-    const l = new FormData(n);
-    d && l.append("file", d);
-    const o = new XMLHttpRequest();
-    o.open("POST", n.action, !0), o.onreadystatechange = function() {
-      o.readyState === 4 && o.status === 200 && (e.querySelector(".message").innerHTML = o.responseText);
-    }, o.send(l);
+  n && n.addEventListener("submit", function(o) {
+    o.preventDefault();
+    const d = new FormData(n);
+    l && d.append("file", l);
+    const e = new XMLHttpRequest();
+    e.open("POST", n.action, !0), e.onreadystatechange = function() {
+      e.readyState === 4 && e.status === 200 && (t.querySelector(".message").innerHTML = e.responseText);
+    }, e.send(d);
   });
 });
