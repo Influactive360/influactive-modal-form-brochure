7663297550b84977ec277f6add3a4c5a658911ec
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-06-16 16:40:22 +0200

    REMOVE file from formdata if ?file=

--- dist/index.mjs
+++ dist/index.mjs
@@ -2,14 +2,14 @@ window.addEventListener("load", function() {
   const r = document.querySelectorAll('a[href*="#brochure"]'), t = document.querySelector("#modal-form"), c = document.querySelector("#modal-form-close");
   let l;
   t && getComputedStyle(t).display === "block" && (document.body.style.overflow = "hidden"), r && t && c && (r.forEach(function(o) {
-    o.addEventListener("click", function(d) {
-      d.preventDefault();
+    o.addEventListener("click", function(n) {
+      n.preventDefault();
       const e = o.getAttribute("href");
       if (e.includes("?")) {
-        const s = e.split("?");
-        if (s.length > 1) {
-          const i = s[1].split("=");
-          i[0] === "file" && (l = i[1]);
+        const i = e.split("?");
+        if (i.length > 1) {
+          const s = i[1].split("=");
+          s[0] === "file" && (l = s[1]);
         }
       }
       t.style.display = "block", document.body.style.overflow = "hidden";
@@ -19,14 +19,14 @@ window.addEventListener("load", function() {
   }), window.addEventListener("click", function(o) {
     o.target === t && (t.style.display = "none", document.body.style.overflow = "auto", document.body.style.overflowX = "hidden");
   }));
-  const n = document.querySelector("#modal-form form");
-  n && n.addEventListener("submit", function(o) {
+  const d = document.querySelector("#modal-form form");
+  d && d.addEventListener("submit", function(o) {
     o.preventDefault();
-    const d = new FormData(n);
-    l && d.append("file", l);
+    const n = new FormData(d);
+    l && (n.delete("file"), n.append("file", l));
     const e = new XMLHttpRequest();
-    e.open("POST", n.action, !0), e.onreadystatechange = function() {
+    e.open("POST", d.action, !0), e.onreadystatechange = function() {
       e.readyState === 4 && e.status === 200 && (t.querySelector(".message").innerHTML = e.responseText);
-    }, e.send(d);
+    }, e.send(n);
   });
 });
