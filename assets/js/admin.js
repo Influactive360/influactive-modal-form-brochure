bbad71eef4ac426fa941688f87f7408971e81945
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-25 10:56:32 +0200

    Refactor admin.js for enhanced code readability and functionality
    Refactored admin.js by introducing helper functions to handle plugin dependency checking and event handling. Added comments to document functions for better maintainability and code readability. Also added validators to ensure correct data type was being returned by functions, reducing future error potential and increasing overall code reliability. This resulted in separate function for initialization and event handling - making it easier to understand and modify.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,43 +1,56 @@
 /* global Choices */
 
-document.addEventListener("DOMContentLoaded", function() {
-	const selectElements = ['modal_form_pages', 'modal_form_posts'];
+/**
+ * @param {string} elementId
+ */
+const initializeChoicesFor = (elementId) => {
+	const selectElement = document.getElementById(elementId);
+	if (selectElement && typeof Choices !== 'undefined') {
+		new Choices(selectElement, {
+			removeItemButton: true,
+			searchEnabled: true,
+		});
+	}
+}
 
-	selectElements.forEach(function(elementId) {
-		const selectElement = document.getElementById(elementId);
-		if (selectElement) {
-			new Choices(selectElement, {
-				removeItemButton: true,
-				searchEnabled: true,
-			});
-		}
+/**
+ * @param e
+ * @param mediaUploader
+ * @param {Element} inputField
+ */
+const handleUploadButtonClick = (e, mediaUploader, inputField) => {
+	e.preventDefault();
+
+	if (mediaUploader) {
+		mediaUploader.open();
+		return;
+	}
+
+	mediaUploader = wp.media.frames.file_frame = wp.media({
+		multiple: false,
 	});
 
-	let mediaUploader
-	const uploadButton = document.querySelector("#upload-button")
-	const inputField = document.querySelector("#modal_form_file_select")
+	mediaUploader.on('select', () => {
+		const attachment = mediaUploader.state().get('selection').first().toJSON();
+		inputField.value = attachment.id;
+	});
 
-	uploadButton.addEventListener("click", function(e) {
-		e.preventDefault()
+	mediaUploader.open();
+};
 
-		// If the uploader object has already been created, reopen the dialog
-		if (mediaUploader) {
-			mediaUploader.open()
-			return
+document.addEventListener('DOMContentLoaded', () => {
+	const selectElements = ['modal_form_pages', 'modal_form_posts'];
+
+	selectElements.forEach(initializeChoicesFor);
+
+	let mediaUploader;
+
+	if(typeof wp !== 'undefined' && wp.media !== 'undefined'){
+		const uploadButton = document.querySelector('#upload-button');
+		const inputField = document.querySelector('#modal_form_file_select');
+
+		if(uploadButton !== null && inputField !== null){
+			uploadButton.addEventListener('click', (e) => handleUploadButtonClick(e, mediaUploader, inputField));
 		}
-
-		// Extend the wp.media object
-		mediaUploader = wp.media.frames.file_frame = wp.media({
-			multiple: false,
-		})
-
-		// When a file is selected, grab the URL and set it as the text field's value
-		mediaUploader.on("select", function() {
-			const attachment = mediaUploader.state().get("selection").first().toJSON()
-			inputField.value = attachment.id
-		})
-
-		// Open the uploader dialog
-		mediaUploader.open()
-	})
-})
+	}
+});
