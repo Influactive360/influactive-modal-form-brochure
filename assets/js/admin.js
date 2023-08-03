6bbb38351432dd14e05aa93733e2e61f5668f911
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-08-03 10:22:27 +0200

    Remove redundant files, integrate SCSS in JS files
    Deleted redundant files related to `assets/js` and `assets/css` in `influactive-modal-form-brochure`. SCSS styling now integrated in JS files directly for more efficient and component-focused styling. This aims to improve code maintainability and project organization.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,16 +1,18 @@
+import '../css/admin-style.scss'
+
 /* global Choices */
 
 /**
  * @param {string} elementId
  */
 const initializeChoicesFor = (elementId) => {
-	const selectElement = document.getElementById(elementId);
-	if (selectElement && typeof Choices !== 'undefined') {
-		new Choices(selectElement, {
-			removeItemButton: true,
-			searchEnabled: true,
-		});
-	}
+  const selectElement = document.getElementById(elementId)
+  if (selectElement && typeof Choices !== 'undefined') {
+    new Choices(selectElement, {
+      removeItemButton: true,
+      searchEnabled: true,
+    })
+  }
 }
 
 /**
@@ -19,38 +21,38 @@ const initializeChoicesFor = (elementId) => {
  * @param {Element} inputField
  */
 const handleUploadButtonClick = (e, mediaUploader, inputField) => {
-	e.preventDefault();
+  e.preventDefault()
 
-	if (mediaUploader) {
-		mediaUploader.open();
-		return;
-	}
+  if (mediaUploader) {
+    mediaUploader.open()
+    return
+  }
 
-	mediaUploader = wp.media.frames.file_frame = wp.media({
-		multiple: false,
-	});
+  mediaUploader = wp.media.frames.file_frame = wp.media({
+    multiple: false,
+  })
 
-	mediaUploader.on('select', () => {
-		const attachment = mediaUploader.state().get('selection').first().toJSON();
-		inputField.value = attachment.id;
-	});
+  mediaUploader.on('select', () => {
+    const attachment = mediaUploader.state().get('selection').first().toJSON()
+    inputField.value = attachment.id
+  })
 
-	mediaUploader.open();
-};
+  mediaUploader.open()
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-	const selectElements = ['modal_form_pages', 'modal_form_posts'];
+  const selectElements = ['modal_form_pages', 'modal_form_posts']
 
-	selectElements.forEach(initializeChoicesFor);
+  selectElements.forEach(initializeChoicesFor)
 
-	let mediaUploader;
+  let mediaUploader
 
-	if(typeof wp !== 'undefined' && wp.media !== 'undefined'){
-		const uploadButton = document.querySelector('#upload-button');
-		const inputField = document.querySelector('#modal_form_file_select');
+  if (typeof wp !== 'undefined' && wp.media !== 'undefined') {
+    const uploadButton = document.querySelector('#upload-button')
+    const inputField = document.querySelector('#modal_form_file_select')
 
-		if(uploadButton !== null && inputField !== null){
-			uploadButton.addEventListener('click', (e) => handleUploadButtonClick(e, mediaUploader, inputField));
-		}
-	}
-});
+    if (uploadButton !== null && inputField !== null) {
+      uploadButton.addEventListener('click', (e) => handleUploadButtonClick(e, mediaUploader, inputField))
+    }
+  }
+})
--- assets/js/modal-form-script.js
+++ assets/js/modal-form-script.js
@@ -1,3 +1,5 @@
+import '../css/modal-form-style.scss'
+
 /* global grecaptcha, ajax_object */
 
 /**
@@ -7,130 +9,130 @@
  * @param {T|string|Blob} recaptchaResponse
  */
 function submitForm(messageDiv, form, file, recaptchaResponse) {
-	const xhr = new XMLHttpRequest();
-	const formData = new FormData(form);
-	formData.append('action', 'send_email');
+  const xhr = new XMLHttpRequest()
+  const formData = new FormData(form)
+  formData.append('action', 'send_email')
 
-	if (recaptchaResponse) {
-		formData.append('recaptcha_response', recaptchaResponse);
-	}
+  if (recaptchaResponse) {
+    formData.append('recaptcha_response', recaptchaResponse)
+  }
 
-	if (file) {
-		formData.delete("brochure")
-		formData.append("brochure", file)
-	}
+  if (file) {
+    formData.delete('brochure')
+    formData.append('brochure', file)
+  }
 
-	xhr.open('POST', ajax_object.ajaxurl, true);
+  xhr.open('POST', ajax_object.ajaxurl, true)
 
-	xhr.onload = function() {
-		if (xhr.status === 200) {
-			const response = JSON.parse(xhr.responseText);
-			if (response.data) {
-				// Display the success message in the div
-				messageDiv.textContent = response.data.message;
-				form.reset();
-			} else {
-				// Display the error message in the div
-				messageDiv.textContent = response.data.message;
-				console.log(response.data);
-			}
-		} else {
-			// Display the AJAX error message in the div
-			messageDiv.textContent = "An error occurred with the AJAX request";
-		}
-	};
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText)
+      if (response.data) {
+        // Display the success message in the div
+        messageDiv.textContent = response.data.message
+        form.reset()
+      } else {
+        // Display the error message in the div
+        messageDiv.textContent = response.data.message
+        console.log(response.data)
+      }
+    } else {
+      // Display the AJAX error message in the div
+      messageDiv.textContent = 'An error occurred with the AJAX request'
+    }
+  }
 
-	xhr.send(formData);
+  xhr.send(formData)
 }
 
-window.addEventListener("load", function() {
-	const brochureLinks = document.querySelectorAll("a[href*=\"#brochure\"]")
-	const modalForm = document.querySelector("#modal-form")
-	const modalFormClose = document.querySelector("#modal-form-close")
-	let file // variable for storing the file URL
+window.addEventListener('load', () => {
+  const brochureLinks = document.querySelectorAll('a[href*="#brochure"]')
+  const modalForm = document.querySelector('#modal-form')
+  const modalFormClose = document.querySelector('#modal-form-close')
+  let file // variable for storing the file URL
 
-	// Check if modalForm is initially in 'block' display
-	if (modalForm && getComputedStyle(modalForm).display === "block") {
-		document.body.style.overflow = "hidden"
-	}
+  // Check if modalForm is initially in 'block' display
+  if (modalForm && getComputedStyle(modalForm).display === 'block') {
+    document.body.style.overflow = 'hidden'
+  }
 
-	if (brochureLinks && modalForm && modalFormClose) {
-		brochureLinks.forEach(function(element) {
-			element.addEventListener("click", function(e) {
-				e.preventDefault()
+  if (brochureLinks && modalForm && modalFormClose) {
+    brochureLinks.forEach((element) => {
+      element.addEventListener('click', (e) => {
+        e.preventDefault()
 
-				// Retrieve the full href including query parameters
-				const href = element.getAttribute("href")
+        // Retrieve the full href including query parameters
+        const href = element.getAttribute('href')
 
-				// Check if href contains '?'
-				if (href.includes("?")) {
-					// Split the href on the '?' character to get the parameters
-					const parts = href.split("?")
+        // Check if href contains '?'
+        if (href.includes('?')) {
+          // Split the href on the '?' character to get the parameters
+          const parts = href.split('?')
 
-					// Check if we have parameters
-					if (parts.length > 1) {
-						// Split the parameters on the '=' character to get the file URL
-						const params = parts[1].split("=")
+          // Check if we have parameters
+          if (parts.length > 1) {
+            // Split the parameters on the '=' character to get the file URL
+            const params = parts[1].split('=')
 
-						// Check if we have a file parameter
-						if (params[0] === "file") {
-							file = params[1]
-						}
-					}
-				}
+            // Check if we have a file parameter
+            if (params[0] === 'file') {
+              file = params[1]
+            }
+          }
+        }
 
-				modalForm.style.display = "block"
-				document.body.style.overflow = "hidden"
-			})
-		})
+        modalForm.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+      })
+    })
 
-		modalFormClose.addEventListener("click", function() {
-			modalForm.style.display = "none"
-			document.body.style.overflow = "auto"
-			document.body.style.overflowX = "hidden"
-		})
+    modalFormClose.addEventListener('click', () => {
+      modalForm.style.display = 'none'
+      document.body.style.overflow = 'auto'
+      document.body.style.overflowX = 'hidden'
+    })
 
-		window.addEventListener("click", function(e) {
-			if (e.target === modalForm) {
-				modalForm.style.display = "none"
-				document.body.style.overflow = "auto"
-				document.body.style.overflowX = "hidden"
-			}
-		})
+    window.addEventListener('click', (e) => {
+      if (e.target === modalForm) {
+        modalForm.style.display = 'none'
+        document.body.style.overflow = 'auto'
+        document.body.style.overflowX = 'hidden'
+      }
+    })
 
-		window.addEventListener("keydown", function(e) {
-			if (e.key === "Escape") {
-				modalForm.style.display = "none"
-				document.body.style.overflow = "auto"
-				document.body.style.overflowX = "hidden"
-			}
-		})
-	}
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        modalForm.style.display = 'none'
+        document.body.style.overflow = 'auto'
+        document.body.style.overflowX = 'hidden'
+      }
+    })
+  }
 
-	// on form submit
-	const form = document.querySelector("#modal-form form")
-	if (form) {
-		form.addEventListener("submit", function(e) {
-			e.preventDefault()
-			const recaptchaInput = form.querySelector('input[name="recaptcha_site_key"]');
-			const messageDiv = form.querySelector('.influactive-form-message');
+  // on form submit
+  const form = document.querySelector('#modal-form form')
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const recaptchaInput = form.querySelector('input[name="recaptcha_site_key"]')
+      const messageDiv = form.querySelector('.influactive-form-message')
 
-			if (recaptchaInput && grecaptcha) {
-				const recaptcha_site_key = recaptchaInput.value;
-				grecaptcha.ready(function() {
-					grecaptcha.execute(recaptcha_site_key, {action: 'submit'}).then(function(token) {
-						submitForm(messageDiv, form, file, token);
-						setTimeout(function() {
-							messageDiv.textContent = '';
-						}, 5000);
-					});
-				});
-			} else {
-				submitForm(messageDiv, form, file, null);
-				setTimeout(function() {
-					messageDiv.textContent = '';
-				}, 5000);
-			}
-		});
-	}
+      if (recaptchaInput && grecaptcha) {
+        const recaptcha_site_key = recaptchaInput.value
+        grecaptcha.ready(() => {
+          grecaptcha.execute(recaptcha_site_key, { action: 'submit' }).then((token) => {
+            submitForm(messageDiv, form, file, token)
+            setTimeout(() => {
+              messageDiv.textContent = ''
+            }, 5000)
+          })
+        })
+      } else {
+        submitForm(messageDiv, form, file, null)
+        setTimeout(() => {
+          messageDiv.textContent = ''
+        }, 5000)
+      }
+    })
+  }
 })
