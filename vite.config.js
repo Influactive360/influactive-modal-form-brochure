57f8c027334abd06d30c5ae86d05d45f449e83d3
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-08-03 10:32:32 +0200

    Add bundled CSS, modal-form-script, frontEnd, backEnd and admin scripts
    Bundled CSS, JavaScript modal-form-script, frontEnd, backEnd and other admin scripts were added. Files were added as part of the refactoring and optimization process to reduce the number of requests, improve code readability and maintainability. Erased redundant entry in the vite configuration file to streamline and optimize the build process.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        frontEnd: 'front-end/frontForm.js',
-        backEndStyle: 'back-end/post-type/style.scss',
-        backEndForm: 'back-end/post-type/form/backForm.js',
-        backEndLayout: 'back-end/post-type/layout/layout.js',
-        backEndTab: 'back-end/post-type/tab/tab.js',
+        frontEnd: 'assets/js/modal-form-script.js',
+        backEnd: 'assets/js/admin.js',
       },
       output: {
         entryFileNames: '[name].bundled.js',
